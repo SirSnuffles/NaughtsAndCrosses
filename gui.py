08b1913eb7e6c0511f3bec0c1e376fdcefc83c21
SirSnuffles <theunknownseven7777@gmail.com>
2019-08-12 12:41:09 +1200

    OOP initialisation

--- gui.py
+++ gui.py
@@ -1,106 +1,119 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-root = tk.Tk()
-frame = tk.Frame(root, background="black")
-frame.pack()
-emptyImage = Image.open("emptyBox.png")
-emptyPhoto = ImageTk.PhotoImage(emptyImage)
 
-crossImage = Image.open("Cross.png")
-PlayCross = ImageTk.PhotoImage(crossImage)
-naughtImage = Image.open("Naught.png")
-PlayNaught = ImageTk.PhotoImage(naughtImage)
 
-playerMove = "Cross"
-moveCount = 0
-board = [['' for x in range(3)] for y in range(3)]
+class board(object):
+	def __init__(self):
+		self.root = tk.Tk()
+		self.frame = tk.Frame(self.root, background= "black")
+		self.frame.pack()
+		self.emptyImage = Image.open("emptyBox.png")
+		self.emptyPhoto = ImageTk.PhotoImage(self.emptyImage)
 
-def alternateMove(butNum):
-	global moveCount
-	global playerMove
-	global board
-	ifWon = returnWin(board)
+		self.crossImage = Image.open("Cross.png")
+		self.PlayCross = ImageTk.PhotoImage(self.crossImage)
+		self.naughtImage = Image.open("Naught.png")
+		self.PlayNaught = ImageTk.PhotoImage(self.naughtImage)
 
-	if moveCount < 9 and ifWon == False:
-		moveCount += 1
-		# board[]
-		if playerMove == "Cross":
-			playerMove = "Naught"
-			return PlayCross
-		elif playerMove == "Naught":
-			playerMove = "Cross"
-			return PlayNaught
-	else:
-		print(ifWon)
-		return
+		self.playerMove = "Cross"
+		self.moveCount = 0
+		self.board = [['' for x in range(3)] for y in range(3)]
+		
+		self.setupButtons()
+		self.root.mainloop()
 
-def returnWin(board):
-	firstDiagonal = [r[i] for i, r in enumerate(board)]
-	secondDiagonl = [r[-i-1] for i, r in enumerate(board)]
+	def setupButtons(self):
+		self.button1 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(1))
+		self.button2 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(2))
+		self.button3 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(3))
+		self.button4 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(4))
+		self.button5 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(5))
+		self.button6 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(6))
+		self.button7 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(7))
+		self.button8 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(8))
+		self.button9 = tk.Button(self.frame, image = self.emptyPhoto, command = lambda: self.playMove(9))
 
-	if all(i == 'x' for i in firstDiagonal):
-		return "Crosses Win"
-	elif all(i == 'x' for i in secondDiagonl):
-		return "Crosses Win"
-	elif all(i == 'o' for i in firstDiagonal):
-		return "Naughts Win"
-	elif all(i == 'o' for i in secondDiagonl):
-		return "Naughts Win"
-	elif all(i == 'x' for i in board[0]):
-		return "Crosses Win"
-	elif all(i == 'x' for i in board[1]):
-		return "Crosses Win"
-	elif all(i == 'x' for i in board[2]):
-		return "Crosses Win"
-	elif all(i == 'o' for i in board[0]):
-		return "Naughts Win"
-	elif all(i == 'o' for i in board[1]):
-		return "Naughts Win"
-	elif all(i == 'o' for i in board[2]):
-		return "Naughts Win"	
-	else:
-		return False
+		self.button1.grid(row = 0, column = 0)
+		self.button2.grid(row = 0, column = 1)
+		self.button3.grid(row = 0, column = 2)
+		self.button4.grid(row = 1, column = 0)
+		self.button5.grid(row = 1, column = 1)
+		self.button6.grid(row = 1, column = 2)
+		self.button7.grid(row = 2, column = 0)
+		self.button8.grid(row = 2, column = 1)
+		self.button9.grid(row = 2, column = 2)
 
-def playMove(num):
+	def alternateMove(self, butNum):
+		ifWon = self.returnWin()
 
-	if num == 1:
-		button1.config(image=alternateMove(num))
-	elif num == 2:
-		button2.config(image=alternateMove(num))
-	elif num == 3:
-		button3.config(image=alternateMove(num))
-	elif num == 4:
-		button4.config(image=alternateMove(num))
-	elif num == 5:
-		button5.config(image=alternateMove(num))
-	elif num == 6:
-		button6.config(image=alternateMove(num))
-	elif num == 7:
-		button7.config(image=alternateMove(num))
-	elif num == 8:
-		button8.config(image=alternateMove(num))
-	elif num == 9:
-		button9.config(image=alternateMove(num))
+		if self.moveCount < 9 and ifWon == False:
+			self.moveCount += 1
+			if self.playerMove == "Cross":
+				self.playerMove = "Naught"
+				return self.PlayCross
+			elif self.playerMove == "Naught":
+				self.playerMove = "Cross"
+				return self.PlayNaught
+		else:
+			print(ifWon)
+			return
 
-button1 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(1))
-button2 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(2))
-button3 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(3))
-button4 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(4))
-button5 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(5))
-button6 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(6))
-button7 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(7))
-button8 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(8))
-button9 = tk.Button(frame, image = emptyPhoto, command = lambda: playMove(9))
+	def returnWin(self):
+		firstDiagonal = [r[i] for i, r in enumerate(self.board)]
+		secondDiagonl = [r[-i-1] for i, r in enumerate(self.board)]
 
-button1.grid(row = 0, column = 0)
-button2.grid(row = 0, column = 1)
-button3.grid(row = 0, column = 2)
-button4.grid(row = 1, column = 0)
-button5.grid(row = 1, column = 1)
-button6.grid(row = 1, column = 2)
-button7.grid(row = 2, column = 0)
-button8.grid(row = 2, column = 1)
-button9.grid(row = 2, column = 2)
+		if all(i == 'x' for i in firstDiagonal):
+			return "Crosses Win"
+		elif all(i == 'x' for i in secondDiagonl):
+			return "Crosses Win"
+		elif all(i == 'o' for i in firstDiagonal):
+			return "Naughts Win"
+		elif all(i == 'o' for i in secondDiagonl):
+			return "Naughts Win"
+		elif all(i == 'x' for i in self.board[0]):
+			return "Crosses Win"
+		elif all(i == 'x' for i in self.board[1]):
+			return "Crosses Win"
+		elif all(i == 'x' for i in self.board[2]):
+			return "Crosses Win"
+		elif all(i == 'o' for i in self.board[0]):
+			return "Naughts Win"
+		elif all(i == 'o' for i in self.board[1]):
+			return "Naughts Win"
+		elif all(i == 'o' for i in self.board[2]):
+			return "Naughts Win"	
+		else:
+			return False
 
-root.mainloop()
+	def playMove(self, num):
+
+		if num == 1:
+			self.button1.config(image=self.alternateMove(num))
+		elif num == 2:
+			self.button2.config(image=self.alternateMove(num))
+		elif num == 3:
+			self.button3.config(image=self.alternateMove(num))
+		elif num == 4:
+			self.button4.config(image=self.alternateMove(num))
+		elif num == 5:
+			self.button5.config(image=self.alternateMove(num))
+		elif num == 6:
+			self.button6.config(image=self.alternateMove(num))
+		elif num == 7:
+			self.button7.config(image=self.alternateMove(num))
+		elif num == 8:
+			self.button8.config(image=self.alternateMove(num))
+		elif num == 9:
+			self.button9.config(image=self.alternateMove(num))
+
+class player():
+	def __init__(self):
+		pass
+
+def main():
+	newGame = board()
+
+if __name__ == '__main__':
+	main()
+
